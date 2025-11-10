974fb7b534743d28f971b6ff6946bb5d0905dc78
AlfredoCorvi <al.cor.vi05@gmail.com>
2025-11-10 17:43:42 -0600

    pagina de lidia añadida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { Alfredo } from './pages/alfredo/alfredo';
 import { Renata } from './pages/renata/renata';
 import { Abigail } from './pages/abigail/abigail';
+import { Lidia } from './pages/lidia/lidia';
 
 export const routes: Routes = [
     {path: '', component:Home},
     {path: 'alfredo', component:Alfredo},
     {path: 'renata', component:Renata},
     {path: 'abigail', component:Abigail},
+    {path: 'lidia', component:Lidia},
     {path: '**', redirectTo:''}
 ];
