bc10df06797bf156c2e6c9a440ce0e32b79d5d2c
AlfredoCorvi <al.cor.vi05@gmail.com>
2025-11-10 17:28:48 -0600

    pagina de abigail tovar añadida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Alfredo } from './pages/alfredo/alfredo';
 import { Renata } from './pages/renata/renata';
+import { Abigail } from './pages/abigail/abigail';
 
 export const routes: Routes = [
     {path: '', component:Home},
     {path: 'alfredo', component:Alfredo},
     {path: 'renata', component:Renata},
+    {path: 'abigail', component:Abigail},
     {path: '**', redirectTo:''}
 ];
