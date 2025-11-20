5cc5b6eb72d681a4ea67628b62fbab1af4132f84
AlfredoCorvi <al.cor.vi05@gmail.com>
2025-11-19 18:42:32 -0600

    pagina de antonio agregada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Alfredo } from './pages/alfredo/alfredo';
 import { Renata } from './pages/renata/renata';
 import { Abigail } from './pages/abigail/abigail';
 import { Lidia } from './pages/lidia/lidia';
+import { Antonio } from './pages/antonio/antonio';
 
 export const routes: Routes = [
     {path: '', component:Home},
@@ -11,5 +12,6 @@ export const routes: Routes = [
     {path: 'renata', component:Renata},
     {path: 'abigail', component:Abigail},
     {path: 'lidia', component:Lidia},
+    {path: 'antonio', component:Antonio},
     {path: '**', redirectTo:''}
 ];
